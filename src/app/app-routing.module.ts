b205363ee4a072f79e7135378e19071f9195779c
Amaral <dmrramaral@gmail.com>
2023-04-28 16:37:28 -0300

    atualização

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './public/login/login.component';
+import { PublicComponent } from './public/public.component';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent },
+  {path: '', component: PublicComponent}
+ /*  { path: '', loadChildren: () => import('./public/public.module').then(m => m.PublicModule) },
+   */
  /*  { path: 'login', loadChildren: () => import('.public/login/login.module').then(m => m.LoginModule)} */
 ];
 
