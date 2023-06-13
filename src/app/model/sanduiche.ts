467d1245844a2a66b16e66831658b9b14ce2b77e
Amaral <dmrramaral@gmail.com>
2023-06-13 16:32:03 -0300

    Ajusta no componente de promocão

--- src/app/model/sanduiche.ts
+++ src/app/model/sanduiche.ts
@@ -3,13 +3,13 @@ import { Ingredientes } from "./ingredientes";
 export class Sanduiche {
 
     public id: Number
-    public nome: String ;
+    public nomeSanduiche: String ;
     public ingredientes : Ingredientes[];
     public valorSanduiche: Number;
 
-    constructor(id: Number, nome: String, ingredientes: Ingredientes[], valorSanduiche: Number) {
+    constructor(id: Number, nomeSanduiche: String, ingredientes: Ingredientes[], valorSanduiche: Number) {
         this.id = id;
-        this.nome = nome;
+        this.nomeSanduiche = nomeSanduiche;
         this.ingredientes = ingredientes;
         this.valorSanduiche = valorSanduiche;
     }
