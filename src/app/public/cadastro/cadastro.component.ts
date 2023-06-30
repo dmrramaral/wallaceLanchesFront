a528f3befe3a027d6a17f93161bc46027d3a66e7
Amaral <dmrramaral@gmail.com>
2023-06-30 16:38:37 -0300

    Corrigindo erro ao salvar

--- src/app/public/cadastro/cadastro.component.ts
+++ src/app/public/cadastro/cadastro.component.ts
@@ -7,21 +7,21 @@ import { CadastroService } from './cadastro.service';
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
   styleUrls: ['./cadastro.component.css'],
-  providers : [CadastroService]
+  providers: [CadastroService]
 })
 export class CadastroComponent implements OnInit {
 
-   cadastroForm!: FormGroup;
+  cadastroForm!: FormGroup;
 
-  private cliente!: Cliente;
+  cliente!: Cliente;
 
   constructor(
     private formBuilder: FormBuilder,
     private cadastroService: CadastroService,
-  ) { 
-    
-    
-    
+  ) {
+
+
+
   }
 
   ngOnInit(): void {
@@ -31,27 +31,26 @@ export class CadastroComponent implements OnInit {
 
 
   onSubmit(): void {
-    console.log(this.cadastroForm?.value);
-    console.log(this.cliente.nome)
-    this.cadastroService.cadastrar(this.cadastroForm.value).subscribe(
-      data => {
+
+    this.cadastroService.cadastrar(this.cadastroForm.value).subscribe({
+      next: (data) => {
         alert("Cliente cadastrado com sucesso!");
         this.cadastroForm?.reset();
       },
-      err => {
+      error: (err) => {
         alert("Erro ao cadastrar cliente!");
       }
-    );
- 
+    });
+
   }
 
-  private createForm(){
+  private createForm() {
     this.cadastroForm = this.formBuilder.group({
-      nome: this.formBuilder.control('',[Validators.required, Validators.minLength(3), Validators.maxLength(100)]),
-      telefone: this.formBuilder.control('',[Validators.required, Validators.pattern('[0-9]{4}-[0-9]{4}')]),
-      cpf: this.formBuilder.control('',[Validators.required, Validators.pattern('[0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}')]),
-      email: this.formBuilder.control('',[Validators.required, Validators.email]),
-      password: this.formBuilder.control('',[Validators.required, Validators.minLength(6), Validators.maxLength(20)])
+      nome: this.formBuilder.control('', [Validators.required, Validators.minLength(3), Validators.maxLength(100)]),
+      telefone: this.formBuilder.control('', [Validators.required, Validators.pattern('[0-9]{4}-[0-9]{4}')]),
+      cpf: this.formBuilder.control('', [Validators.required, Validators.pattern('[0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}')]),
+      email: this.formBuilder.control('', [Validators.required, Validators.email]),
+      password: this.formBuilder.control('', [Validators.required, Validators.minLength(6), Validators.maxLength(20)])
     });
   }
 
